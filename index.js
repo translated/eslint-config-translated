2df74486dd970a3354681f219a45c925a631e89f
emilioSp <emilio.spatola@gmail.com>
2021-05-17 17:13:10 +0200

    fix: add babel parser

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-export default {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "jest": true,
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    node: true
   },
-  "parserOptions": {
-    "ecmaVersion": 2021,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "prettier",
-    "prettier/react",
-  ],
-  "plugins": ["prettier", "react", "react-hooks"],
-  "rules": {
-    "prettier/prettier": [
-      "error",
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'react', 'react-hooks'],
+  rules: {
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "printWidth": 160
+        singleQuote: true,
+        printWidth: 160,
       },
     ],
-    "no-console": "off",
-    "no-unused-vars": "off",
+    'no-console': 'off',
+    'no-unused-vars': 'off',
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
