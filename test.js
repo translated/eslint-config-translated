dbc59743ac51ad929cf4763f7a289ada76657b69
damiano.barbati <damiano.barbati@gmail.com>
2021-10-21 13:11:06 +0200

    remove smelling of emilio, no jest and react deps bringing a lots of explosions with audit

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
+const { strict: assert } = require('assert');
 const { ESLint } = require('eslint');
+
 const config = require('./index.js');
 
-describe('eslint-config-translated', () => {
-  test('loads config correctly', async () => {
-    const eslint = new ESLint({
-      overrideConfig: config
-    });
-    const code = `const foo = "bar";`;
-    const [result] = await eslint.lintText(code);
-    const rules = result.messages.map(m => m.ruleId);
-    expect(rules.includes('no-unused-vars')).toBeTruthy();
-    expect(rules.includes('prettier/prettier')).toBeTruthy();
-  })
-})
+const eslint = new ESLint({
+  overrideConfig: config
+});
+
+(async () => {
+  const code = `const foo = "bar";`;
+  const [result] = await eslint.lintText(code);
+  const rules = result.messages.map(m => m.ruleId);
+
+  assert.deepEqual(rules.includes('no-unused-vars'), true);
+  assert.deepEqual(rules.includes('prettier/prettier'), true);
+})();
